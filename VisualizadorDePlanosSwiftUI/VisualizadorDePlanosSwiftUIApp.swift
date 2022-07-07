7fb7ff7fabdb0f2f4341e51e4c922f306792090a
Manuel Moyano <manuelm_98@hotmail.com>
2022-07-07 11:19:46 -0300

    Initial Commit

---
+++ VisualizadorDePlanosSwiftUI/VisualizadorDePlanosSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualizadorDePlanosSwiftUIApp.swift
+//  VisualizadorDePlanosSwiftUI
+//
+//  Created by Manuel Moyano on 07/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct VisualizadorDePlanosSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
